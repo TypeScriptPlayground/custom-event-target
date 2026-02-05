79e4ddb2fd003f1d51ca2c6b1ea83eea8c495c56
Mqxx <62719703+Mqxx@users.noreply.github.com>
2026-02-05 16:37:01 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+/**
+ * This module contains a class and types for a `CustomEventTarget`.
+ * 
+ * @example
+ * ```ts
+ * class MyClass extends CustomEventTarget { ... }
+ * ```
+ * @example
+ * ```ts
+ * const target = new CustomEventTarget();
+ * ```
+ * @module
+ */
 
+export * from './custom_event_target.ts'
